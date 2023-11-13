ae98b61746cc3b5f798da4c8c404dfa9fc9048d9
Jessica Amos <amosjessicar@gmail.com>
2023-11-13 11:43:09 -0500

    e4-e6 complete

--- exercises/e5.js
+++ exercises/e5.js
@@ -6,7 +6,12 @@
 
 export function getAllClientNames(array) {
   // Your code goes here...
+  const names = [];
 
+  for(let i = 0; i < array.length; i++){
+    names.push(array[i].name);
+  }
+  return names;
 }
 
 
--- exercises/e6.js
+++ exercises/e6.js
@@ -5,7 +5,13 @@
 
 export function getClientWithNoMoney(array) {
   // Your code goes here...
-
+  const emptyBalance = [];
+  for(let i = 0; i < array.length; i++){
+    if(array[i].balance === 0){
+      emptyBalance.push(array[i].name);
+    }
+  }
+  return emptyBalance;
 }
 
 // === TEST YOURSELF ===
